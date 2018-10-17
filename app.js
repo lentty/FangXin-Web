ca83620a45cef730835e055f8674544b288cc9a7
mango <mindyhuang01@163.com>
2018-10-18 00:01:46 +0800

    beautify search bar

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 //app.js
 App({
   globalData: {
-    //host: "https://api.fangxincq.com/"
-    host: "http://localhost:8080/"
+    host: "https://api.fangxincq.com/"
+   // host: "http://localhost:8080/"
   },
   data: {
-    //host: "https://api.fangxincq.com/"
-    host: "http://localhost:8080/"
+    host: "https://api.fangxincq.com/"
+   // host: "http://localhost:8080/"
   },    
   onLaunch: function () {
   }
